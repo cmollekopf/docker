e0e56ff5805421ec13559448fad5652b4e16f10b
Christian Mollekopf <chrigi_1@fastmail.fm>
2015-05-20 17:49:21 +0200

    kontact: working authentication and a default kolab user

--- kontact/run.sh
+++ kontact/run.sh
@@ -9,7 +9,7 @@ touch $XAUTH
 xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
 
 #assumes the container is running
-CONTAINER=$(docker ps -a | grep kolab1:ready | head -n 1 | awk '{ print $1 }')
+CONTAINER=$(docker ps -a | grep kolab/kolabtestcontainer:latest | head -n 1 | awk '{ print $1 }')
 
 # docker run --rm -ti -u developer -e DISPLAY=$DISPLAY -e XAUTHORITY=$XAUTH --device=/dev/dri/card0:/dev/dri/card0 -v /tmp/.X11-unix:/tmp/.X11-unix -v $KDEROOT:/opt/kde kontact:latest /bin/bash
 docker run --rm -ti --link $CONTAINER:kolab -u developer -e DISPLAY=$DISPLAY -e XAUTHORITY=$XAUTH --device=/dev/dri/card0:/dev/dri/card0 -v /tmp/.X11-unix:/tmp/.X11-unix -v $KDEROOT:/opt/kde kontact:latest /bin/bash

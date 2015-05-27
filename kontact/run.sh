d22a45fb3b0a61877d5d8e33fb1789b6f148f68f
Christian Mollekopf <chrigi_1@fastmail.fm>
2015-05-27 17:48:59 +0200

    Mount xauthority file

--- kontact/run.sh
+++ kontact/run.sh
@@ -13,4 +13,4 @@ xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
 CONTAINER=$(docker ps -a | grep kolab/kolabtestcontainer:latest | head -n 1 | awk '{ print $1 }')
 
 # docker run --rm -ti -u developer -e DISPLAY=$DISPLAY -e XAUTHORITY=$XAUTH --device=/dev/dri/card0:/dev/dri/card0 -v /tmp/.X11-unix:/tmp/.X11-unix -v $KDEROOT:/opt/kde kontact:latest /bin/bash
-docker run --rm -ti --link $CONTAINER:kolab -u developer -e DISPLAY=$DISPLAY -e XAUTHORITY=$XAUTH --device=/dev/dri/card0:/dev/dri/card0 -v /tmp/.X11-unix:/tmp/.X11-unix -v $KDEROOT:/opt/kde kontact:latest /bin/bash
+docker run --rm -ti --link $CONTAINER:kolab -u developer -e DISPLAY=$DISPLAY -e XAUTHORITY=$XAUTH --device=/dev/dri/card0:/dev/dri/card0 -v $XAUTH:$XAUTH -v /tmp/.X11-unix:/tmp/.X11-unix -v $KDEROOT:/opt/kde kontact:latest /bin/bash

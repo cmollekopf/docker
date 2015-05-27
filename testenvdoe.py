6f5a2c59c1e84b95e7a8a128d236bfc64e334e72
Christian Mollekopf <chrigi_1@fastmail.fm>
2015-05-27 13:02:29 +0200

    Test script to setup kolab and kontact container

---
+++ testenvdoe.py
@@ -0,0 +1,24 @@
+#!/bin/python2
+import sh
+import subprocess
+
+def findContainer(name):
+    container=""
+    try:
+        container = sh.awk("{print $1}", _in=sh.head("-n 1", _in=sh.grep(name, _in=sh.docker("ps", "-a"))))
+    except:
+        print "container not available"
+    return container.rstrip()
+
+containerName="kolab/kolabtestcontainer:latest"
+container=findContainer(containerName)
+
+if not container:
+    print "starting container"
+    subprocess.call("./run.sh", shell=True, cwd="kolab")
+    container=findContainer(containerName)
+
+print container
+subprocess.call("./run.sh", shell=True, cwd="kontact")
+sh.docker("stop", container)
+sh.docker("rm", container)

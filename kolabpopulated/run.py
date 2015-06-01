47f23d762d673fb8f2edc3bbf47b474609ae18c3
Christian Mollekopf <chrigi_1@fastmail.fm>
2015-06-01 14:45:06 +0200

    Only bind port 80 by default

--- kolabpopulated/run.py
+++ kolabpopulated/run.py
@@ -8,10 +8,10 @@ def main(dataset):
     runargs = (
         "-d",
         "-p", "80:80",
-        "-p", "143:143",
-        "-p", "443:443",
-        "-p", "587:587",
-        "-p", "4190:4190",
+        # "-p", "143:143",
+        # "-p", "443:443",
+        # "-p", "587:587",
+        # "-p", "4190:4190",
         "-h", settings.HOSTNAME,
         "-v", "/sys/fs/cgroup:/sys/fs/cgroup:ro",
         settings.kolabimagename(settings.populatedTag(dataset))

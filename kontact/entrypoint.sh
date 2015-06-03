18a1504b163a2caf1c3fe13337a1b04dd8d3b113
Christian Mollekopf <chrigi_1@fastmail.fm>
2015-06-03 10:20:46 +0200

    Debug output from the beginning

--- kontact/entrypoint.sh
+++ kontact/entrypoint.sh
@@ -7,9 +7,14 @@ export QT_X11_NO_MITSHM=1
 sudo setfacl -m user:developer:rw /dev/dri/card0
 
 kwalletd&
+export IMAP_TRACE=1
 akonadictl start &> /tmp/akonadi.output
 dbus-monitor --session interface='org.freedesktop.Notifications',member='Notify' &> /tmp/notifications.output &
 
 # Block until akonadi is started so we don't accidentaly start akonadi again
 while ! qdbus org.freedesktop.Akonadi.Control / org.freedesktop.DBus.Peer.Ping; do sleep 1 && echo sleep; done; echo "done"
 while ! qdbus org.freedesktop.Akonadi.Resource.akonadi_kolab_resource_0 / org.freedesktop.DBus.Peer.Ping; do sleep 1 && echo sleep; done; echo "done"
+
+function syncCollection() {
+    qdbus org.freedesktop.Akonadi.Resource.akonadi_kolab_resource_0 / org.freedesktop.Akonadi.Resource.synchronizeCollection $1
+}

0edc43c72af5ed6659f5ee09fb1b398faa5c91cd
Christian Mollekopf <chrigi_1@fastmail.fm>
2015-06-23 09:25:28 +0200

    Let's see if plumbum works better than sh.py

--- kolab/build.py
+++ kolab/build.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python2
-from sh import docker
 import subprocess
 import time
 import os
 import sys
+from plumbum.cmd import docker
+from plumbum import FG
 
 import settings
-
-def process_output(line):
-    print(line)
 
 def main():
     tmpname = settings.kolabimagename("tmpbase")
@@ -16,19 +14,18 @@ def main():
     SCRIPT_DIR = os.path.dirname(os.path.realpath(sys.argv[0]))
 
     print("Building tmpcontainer...")
-    docker.build("--no-cache=true", "-t", tmpname, SCRIPT_DIR+"/kolab/", _out=process_output)
+    docker["build", "--no-cache=true", "-t", tmpname, SCRIPT_DIR+"/kolab/"]
     print("Starting tmpcontainer...")
-    print(SCRIPT_DIR+"/fixRoundcubeT243.sh:/usr/share/roundcubemail/fixRoundcubeT243.sh")
-    container = docker.run("-d", "-h", settings.HOSTNAME, "-v", "/sys/fs/cgroup:/sys/fs/cgroup:ro", "-v", SCRIPT_DIR+"/kolab/fixRoundcubeT243.sh:/usr/share/roundcubemail/fixRoundcubeT243.sh", tmpname).rstrip()
+    container = docker["run", "-d", "-h", settings.HOSTNAME, "-v", "/sys/fs/cgroup:/sys/fs/cgroup:ro", "-v", SCRIPT_DIR+"/kolab/fixRoundcubeT243.sh:/usr/share/roundcubemail/fixRoundcubeT243.sh", tmpname]().rstrip()
 
     print("Setting up kolab")
-    docker("exec", container,  "setup-kolab", "--default", "--timezone="+settings.TIMEZONE, "--directory-manager-pwd="+settings.LDAPPW, "--mysqlserver=new", _out=process_output)
+    docker["exec", container,  "setup-kolab", "--default", "--timezone="+settings.TIMEZONE, "--directory-manager-pwd="+settings.LDAPPW, "--mysqlserver=new"]
 
     print("Fixing roundcube")
-    docker("exec", container, "bash", "/usr/share/roundcubemail/fixRoundcubeT243.sh", _out=process_output)
-    docker("exec", container, "systemctl", "restart", "httpd", _out=process_output)
+    docker["exec", container, "bash", "/usr/share/roundcubemail/fixRoundcubeT243.sh"] & FG
+    docker["exec", container, "systemctl", "restart", "httpd"] & FG
 
     print("Comitting results...")
-    docker.commit(container, settings.kolabimagename("base"))
-    docker.stop(container)
-    docker.rm(container)
+    docker["commit", container, settings.kolabimagename("base")]
+    docker["stop", container] & FG
+    docker["rm", container] & FG

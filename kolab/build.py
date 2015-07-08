40b66d1a11801e7acf20c8ec6c02eb85082bf6fa
Christian Mollekopf <chrigi_1@fastmail.fm>
2015-07-08 18:11:45 +0200

    Fixed kolab container setup and patch roundcube to send itip messages

--- kolab/build.py
+++ kolab/build.py
@@ -14,18 +14,25 @@ def main():
     SCRIPT_DIR = os.path.dirname(os.path.realpath(sys.argv[0]))
 
     print("Building tmpcontainer...")
-    docker["build", "--no-cache=true", "-t", tmpname, SCRIPT_DIR+"/kolab/"]
+    # docker["build", "--no-cache=true", "-t", tmpname, SCRIPT_DIR+"/kolab/"] & FG
+    docker["build", "-t", tmpname, SCRIPT_DIR+"/kolab/"] & FG
     print("Starting tmpcontainer...")
-    container = docker["run", "-d", "-h", settings.HOSTNAME, "-v", "/sys/fs/cgroup:/sys/fs/cgroup:ro", "-v", SCRIPT_DIR+"/kolab/fixRoundcubeT243.sh:/usr/share/roundcubemail/fixRoundcubeT243.sh", tmpname]().rstrip()
+    container = docker["run", "-d",
+            "-h", settings.HOSTNAME,
+            "-v", "/sys/fs/cgroup:/sys/fs/cgroup:ro",
+            "-v", SCRIPT_DIR+"/kolab/fixRoundcubeT243.sh:/usr/share/roundcubemail/fixRoundcubeT243.sh",
+            "-v", SCRIPT_DIR+"/kolab/fixRoundcubeT615.patch:/fixRoundcubeT615.patch",
+            tmpname]().rstrip()
 
     print("Setting up kolab")
-    docker["exec", container,  "setup-kolab", "--default", "--timezone="+settings.TIMEZONE, "--directory-manager-pwd="+settings.LDAPPW, "--mysqlserver=new"]
+    docker["exec", container,  "setup-kolab", "--default", "--timezone="+settings.TIMEZONE, "--directory-manager-pwd="+settings.LDAPPW, "--mysqlserver=new"] & FG
 
     print("Fixing roundcube")
     docker["exec", container, "bash", "/usr/share/roundcubemail/fixRoundcubeT243.sh"] & FG
+    docker["exec", container, "patch", "-n", "/usr/share/roundcubemail/plugins/libcalendaring/libvcalendar.php", "/fixRoundcubeT615.patch"] & FG
     docker["exec", container, "systemctl", "restart", "httpd"] & FG
 
     print("Comitting results...")
-    docker["commit", container, settings.kolabimagename("base")]
+    docker["commit", container, settings.kolabimagename("base")] & FG
     docker["stop", container] & FG
     docker["rm", container] & FG

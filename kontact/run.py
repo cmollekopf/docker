5012ff094ff6ada44c5f4c5504100002ac6b1bb8
Christian Mollekopf <chrigi_1@fastmail.fm>
2015-12-01 12:33:07 +0100

    Reuse X11Support

--- kontact/run.py
+++ kontact/run.py
@@ -6,36 +6,29 @@ import os
 
 import settings
 import dockerutils
-
-def setupX11Authorization(xauthFile, display):
-    try:
-        sh.touch(xauthFile)
-    except:
-        print("touch failed")
-    sh.xauth(sh.sed(sh.xauth("nlist", display), "-e", 's/^..../ffff/'), "-f", xauthFile, "nmerge", "-")
+from x11support import X11Support
 
 def main(kolabcontainer, configset):
     containername="kontact"
 
-    DISPLAY = os.environ.get('DISPLAY')
-    XAUTH="/tmp/.docker.xauth"
-    setupX11Authorization(XAUTH, DISPLAY)
-
-    runargs = (
+    runargs = [
         "--rm",
         "-ti",
         "-u", "developer",
         "--link", "{}:kolab".format(kolabcontainer),
-        "-e", "DISPLAY={}".format(DISPLAY),
-        "-e", "XAUTHORITY={}".format(XAUTH),
         "--device", "/dev/dri/card0:/dev/dri/card0",
         "--device", "/dev/dri/renderD128:/dev/dri/renderD128",
         "--device", "/dev/dri/controlD64:/dev/dri/controlD64",
-        "-v", "{}:{}".format(XAUTH, XAUTH),
-        "-v", "/tmp/.X11-unix:/tmp/.X11-unix",
         "-v", "{}:/opt/kde".format(settings.KDEROOT),
+    ]
+
+    x11 = X11Support()
+    x11.setupX11Authorization()
+    runargs.extend(x11.docker_args())
+    runargs.extend([
         "{}:{}".format(containername, configset),
         "/bin/bash"
-    )
+    ]);
+
     # docker.run(*runargs, _out=process_output, _tty_out=True, _tty_in=True)
     subprocess.call("docker run " + " ".join(runargs), shell=True)

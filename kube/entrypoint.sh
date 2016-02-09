4e2894c5f063ba94b236d1b44b224e5b77048466
Christian Mollekopf <chrigi_1@fastmail.fm>
2016-02-09 15:12:01 +0100

    Find installed QML files

--- kube/entrypoint.sh
+++ kube/entrypoint.sh
@@ -4,6 +4,9 @@ export QT_PLUGIN_PATH=/work/install/lib64/plugins/:$QT_PLUGIN_PATH
 export LD_LIBRARY_PATH=/work/install/lib64
 export PATH=/work/install/bin:$PATH
 
+export XDG_CONFIG_DIRS=/work/install/share/config:$XDG_CONFIG_DIRS
+export XDG_DATA_DIRS=/work/install/share/:/usr/share:$XDG_DATA_DIRS
+
 # Use same graphics system as host, since we'll share the X11 socket
 export QT_GRAPHICSSYSTEM=native
 export QT_X11_NO_MITSHM=1

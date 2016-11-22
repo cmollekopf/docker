d80c3a6f480172da5d69fe8b2d53950cc8f23166
Christian Mollekopf <chrigi_1@fastmail.fm>
2016-11-22 13:54:30 +0100

    Create an account for the maildir resource as well

--- kube/default/setupresource.sh
+++ kube/default/setupresource.sh
@@ -1,2 +1,3 @@
-sinksh create resource identifier maildir.instance1 type sink.maildir path /home/developer/maildir1
-sinksh sync maildir.instance1
+sinksh create account type maildir identifier maildiraccount1 name MaildirAccount
+sinksh create resource identifier maildirresource1 account maildiraccount1 type sink.maildir path /home/developer/maildir1
+sinksh sync maildirresource1

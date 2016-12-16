beeb0469bfbc34bcf5b8b52ff6a3626a6c304aec
Christian Mollekopf <chrigi_1@fastmail.fm>
2016-12-16 14:56:26 +0100

    Setup identity with account.

--- kube/default/setupimap.sh
+++ kube/default/setupimap.sh
@@ -1,4 +1,5 @@
 sinksh create account type imap identifier imapaccount1 name ImapAccount
 sinksh create resource type sink.imap identifier imapresource1 account imapaccount1 server imaps://kolab:143 username john.doe@example.org password Welcome2KolabSystems
 sinksh create resource type sink.mailtransport identifier smtpresource1 account imapaccount1 server smtps://kolab:25 username john.doe@example.org password Welcome2KolabSystems
+sinksh create identity name "John Doe" address doe@example.org account imapaccount1
 sinksh sync imapresource1

b06128c3bf7a631adb75a06c54794732a7163683
Sandro Knauß <knauss@kolabsys.com>
2017-01-11 11:17:14 +0100

    make packagetest ubuntu1404 run a kolab resource

--- packagetest/ubuntu1404/kdeenv.sh
+++ packagetest/ubuntu1404/kdeenv.sh
@@ -13,24 +13,6 @@ function akonadidb {
     mysql --auto-vertical-output -S $XDG_DATA_HOME/akonadi/socket-*/mysql.socket akonadi
 }
 
-export KDEDIR=/opt/kde
-export KDEHOME=$HOME/.kde4src/
-export KDETMP=/tmp/$USER-kde4src
-mkdir -p $KDETMP
-export KDESYCOCA=/var/tmp/kdesycoca-custom
-
-export KDEDIRS=$KDEDIR:/usr
-export PATH=$KDEDIR/bin:$PATH
-export LD_LIBRARY_PATH=$KDEDIR/lib:$KDEDIR/lib64
-export PKG_CONFIG_PATH=$KDEDIR/lib/pkgconfig
-export QT_PLUGIN_PATH=$KDEDIR/lib/kde4/plugins:$KDEDIR/lib64/kde4/plugins
-
-# XDG
-export XDG_CONFIG_HOME=$KDEHOME/.config
-export XDG_DATA_HOME=$KDEHOME/.local/share
-export XDG_CONFIG_DIRS=$KDEDIR/share/config
-export XDG_DATA_DIRS=$KDEDIR/share/:/usr/share
-
 export KDE_FULL_SESSION=true
 export KDE_SESSION_UID=`id -ru`
 export KDE_SESSION_VERSION=4

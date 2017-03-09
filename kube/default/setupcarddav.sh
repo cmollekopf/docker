707075cbbc7e91ca03bd58f023b9875bbcb973bb
Christian Mollekopf <chrigi_1@fastmail.fm>
2017-03-09 10:43:11 +0100

    setupscripts

--- kube/default/setupcarddav.sh
+++ kube/default/setupcarddav.sh
@@ -1,3 +1,2 @@
-sinksh create resource type sink.davresource  identifier dav1 resourceUrl https://apps.kolabnow.com/addressbooks/test1%40kolab.org  username test1@kolab.org  password Welcome2KolabSystems
+sinksh create resource type sink.davresource  identifier dav1 resourceUrl kolab/iRony/addressbooks/doe%40example.org  username doe@example.org  password Welcome2KolabSystems
 sinksh sync dav1
-sinksh list folder dav1 --show-all

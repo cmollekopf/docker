9059210882601a31b5d574aafa86e32e41cd4fc7
Christian Mollekopf <chrigi_1@fastmail.fm>
2017-03-14 16:01:48 +0100

    Adjusted setup scripts

--- kube/default/setupkolabnowtest.sh
+++ kube/default/setupkolabnowtest.sh
@@ -1,7 +1,7 @@
 sinksh create account type kolab identifier kolabnowAccount name KolabnowAccount
 sinksh create resource type sink.imap identifier kolabnowImap account kolabnowAccount server imaps://imap.kolabnow.com:993 username test1@kolab.org password Welcome2KolabSystems
 sinksh create resource type sink.mailtransport identifier kolabnowSmtp account kolabnowAccount server smtps://smtp.kolabnow.com:587 username test1@kolab.org password Welcome2KolabSystems
-sinksh create resource type sink.dav  identifier kolabnowDav account kolabnowAccount resourceUrl https://apps.kolabnow.com/addressbooks/test1%40kolab.org  username test1@kolab.org  password Welcome2KolabSystems
+sinksh create resource type sink.dav  identifier kolabnowDav account kolabnowAccount server https://apps.kolabnow.com/addressbooks/test1%40kolab.org  username test1@kolab.org  password Welcome2KolabSystems
 
 sinksh sync kolabnowImap
 sinksh sync kolabnowDav
